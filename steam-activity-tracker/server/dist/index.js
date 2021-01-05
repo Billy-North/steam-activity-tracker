6ccde440791e09547a6bb3728250dd07d8eec26f
Billy-North <william.north@griffithuni.edu.au>
2021-01-05 23:13:42 +1000

    Added postgres to express app

--- steam-activity-tracker/server/dist/index.js
+++ steam-activity-tracker/server/dist/index.js
@@ -1,15 +1,40 @@
 "use strict";
+// import express from "express";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express();
+// const port = 8080;
+// app.listen(port, () => {
+//     console.log(`server started at http://localhost:${port}`);
+// });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const config_1 = require("./config");
 const app = express_1.default();
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(cors_1.default());
 const port = 8080;
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
+});
+// const getBooks = (request: any, response: any) => {
+//     pool.query('SELECT * FROM users', (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//     })
+// }
+app.get('/', (req, res) => {
+    config_1.pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
 });
 //# sourceMappingURL=index.js.map
--- steam-activity-tracker/server/src/index.ts
+++ steam-activity-tracker/server/src/index.ts
@@ -1,8 +1,26 @@
 import express from "express";
-const app = express();
-const port = 8080;
+import bodyParser from "body-parser";
+import cors from "cors";
+import { pool } from "./config";
 
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+
+
+
+const port = 8080;
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
+
+
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) { throw error }
+        res.status(200).json(results.rows)
+    })
+})
